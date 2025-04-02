0b807fab972a33d941af3c838fbfd034cae998fd
Sandesh-772 <jamkatels7@gmail.com>
2025-04-02 12:21:34 +0545

    Add files via upload

---
+++ C#/MyFirstProhect/MyCollegeLab/program10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollegeLab
+{
+    internal class program10
+    {
+        public program10()
+        {
+
+            string message = null;
+            string result = message ?? "My Message";
+            Console.WriteLine(result);
+
+            int[] numbers = null;
+            Console.WriteLine(numbers?.Length ?? 0);
+        }
+    }
+
+}
+}}
